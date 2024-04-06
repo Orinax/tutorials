1a9aecdfc6dcbcf375d4ff36d9d4c9773602d128
Chauncey Pelton <chaunceypelton@gmail.com>
2024-04-06 22:03:27 +0700

    Finished tutorial chapter 3

---
+++ estate/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+    'author': "Chauncey Pelton - Odoo Tutorial",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
